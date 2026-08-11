6185c8cea2389b2d44aad37cbf98622a167e3fcf
Lex Borisoff <lex@borisoff.dev>
2026-08-11 17:59:31 -0400

    chore: update dev dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
-import lexjs from '@lexjs/eslint-plugin';
-import { useIgnoreFile } from '@lexjs/eslint-plugin/utils';
+import lexjs from '@lexjs/eslint';
+import { useIgnoreFile } from '@lexjs/eslint/utils';
 import globals from 'globals';
 import * as tseslint from 'typescript-eslint';
 
